b6eaa6970a60b4e19386aed298e40f2ca563d3de
nosequeponeraquiXD <140538914+nosequeponeraquiXD@users.noreply.github.com>
2026-03-28 12:29:04 -0300

    começo
    isso mesmo xd

---
+++ src/tsp/nearestNeighbor.ts
@@ -0,0 +1,9 @@
+export function nearestNeighbor(matrix: number[][]): number {
+  const n = matrix.length;
+  const visited = new Array(n).fill(false);
+
+  let current = 0;
+  visited[current] = true;
+
+ 
+}
